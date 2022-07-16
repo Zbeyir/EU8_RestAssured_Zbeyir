a892df7f2c6aae91379b0a405f2857fa890343bc
zübeyir <zeynelzatari@gmail.com>
2022-07-16 11:31:23 +0200

    day_2 static import implemented

--- src/test/java/com/cybertek/Day2/HrGetRequests.java
+++ src/test/java/com/cybertek/Day2/HrGetRequests.java
@@ -1,11 +1,18 @@
 package com.cybertek.Day2;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+//import static io.restassured.RestAssured.given; // 2. adim improt ettik 1. adim da kisa yol kullanarak
+import static io.restassured.RestAssured.*; // 3. adim .given() ' i sildik ve onun yerine * yazdik
+//import static org.junit.jupiter.api.Assertions.assertEquals; //2. adim improt ettik 1. adim da kisa yol kullanarak
+import static org.junit.jupiter.api.Assertions.*; //3. adim .assertEquals ' i sildik ve onun yerine * yazdik
+
 
 public class HrGetRequests {
 
@@ -21,7 +28,7 @@ public class HrGetRequests {
     @DisplayName("GET request to /regions")
     @Test
     public void test1() {
-        Response response = RestAssured.get("/regions");
+        Response response = get("/regions");
 
         //print the status code
         System.out.println(response.statusCode());
@@ -38,20 +45,34 @@ public class HrGetRequests {
 
     @DisplayName("GET request to /regions/2 ")
     @Test
-    public void test2(){
+    public void test2() {
 
-        Response response = RestAssured.get("/regions/2");
+        //  Response response = RestAssured.get("/regions");
+        //ben direkt bu yukari da ki gibi yazip gecmistim hata vermedi ama alltaki tam dogru
+        //cünkü bizden .json olarak istiyor
+//=============================================
+
+        //  Response response = RestAssured.given().accept(ContentType.JSON)
+        //                .when()
+        //                  .get("/regions/2");
+
+        Response response = given().accept(ContentType.JSON).
+                when()
+                .get("/regions/2");
+        // bu yukaridan RestAssured. bunu sildik --1.adim
+
 
         //verify status code
-        Assertions.assertEquals(200,response.statusCode());
+        assertEquals(200, response.statusCode());
+        // 1. adim da Assertions. 'ni sildik
 
         //verify content type
-        Assertions.assertEquals("application/json",response.contentType());
+        assertEquals("application/json", response.contentType());
 
         response.prettyPrint();  // bunu yazinca body 'i oldugu gibi alip geliyor
 
         //verify body contains Americas
-        Assertions.assertTrue(response.body().asString().contains("Americas"));
+        assertTrue(response.body().asString().contains("Americas"));
 
 
     }
