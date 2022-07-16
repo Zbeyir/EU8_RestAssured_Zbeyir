b85ec5ff88a06b273625b08e983a7d9d555126a7
zübeyir <zeynelzatari@gmail.com>
2022-07-16 11:15:30 +0200

    day_2 HrGetRequests test 1 is done

---
+++ src/test/java/com/cybertek/Day2/HrGetRequests.java
@@ -0,0 +1,59 @@
+package com.cybertek.Day2;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class HrGetRequests {
+
+    //BeforeAll is a annotation equals to @BeforeClass in testNg, we use with static method name
+    @BeforeAll
+    public static void init() {
+
+        //save baseurl inside this variable so that we dont need to type each http method.
+        RestAssured.baseURI = "http://44.202.63.224:1000/ords/hr";
+
+    }
+
+    @DisplayName("GET request to /regions")
+    @Test
+    public void test1() {
+        Response response = RestAssured.get("/regions");
+
+        //print the status code
+        System.out.println(response.statusCode());
+
+    }
+
+    /*
+        Given accept type is application/json
+        When user sends get request to /regions/2
+        Then response status code must be 200
+        and content type equals to application/json
+        and response body contains   Americas
+     */
+
+    @DisplayName("GET request to /regions/2 ")
+    @Test
+    public void test2(){
+
+        Response response = RestAssured.get("/regions/2");
+
+        //verify status code
+        Assertions.assertEquals(200,response.statusCode());
+
+        //verify content type
+        Assertions.assertEquals("application/json",response.contentType());
+
+        response.prettyPrint();  // bunu yazinca body 'i oldugu gibi alip geliyor
+
+        //verify body contains Americas
+        Assertions.assertTrue(response.body().asString().contains("Americas"));
+
+
+    }
+
+}
