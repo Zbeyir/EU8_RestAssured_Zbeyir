4796c063c4347ff736deb0faf3b7b27b234ab15c
zübeyir <zeynelzatari@gmail.com>
2022-07-22 12:22:10 +0200

    day_3 GET request to /countries with Query Param test1 is done

---
+++ src/test/java/com/cybertek/day3/ORDSApiTestsWithParameters.java
@@ -0,0 +1,55 @@
+package com.cybertek.day3;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class ORDSApiTestsWithParameters {
+
+    @BeforeAll
+    public static void init() {
+
+        //save baseurl inside this variable so that we dont need to type each http method.
+        baseURI = "http://44.202.63.224:1000/ords/hr";
+    }
+
+    /*
+      Given accept type is Json
+        And parameters: q = {"region_id":2}
+        When users sends a GET request to "/countries"
+        Then status code is 200
+        And Content type is application/json
+        And Payload should contain "United States of America"
+     */
+
+    @DisplayName("GET request to /countries with Query Param")
+    @Test
+    public void test1() {
+
+        Response response = given().accept(ContentType.JSON)
+                .and().queryParam("q", "{\"region_id\":2}")
+                .log().all()
+                .when()
+                .get("/countries");
+
+
+        assertEquals(200,response.statusCode());
+
+        assertEquals("application/json",response.header("Content-Type")); // burada bunu degisik bir yolla yaptik
+
+        assertTrue(response.body().asString().contains("United States of America"));
+
+        response.prettyPrint();
+
+
+
+    }
+
+
+}
