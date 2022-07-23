e8659a97fe612f7c120d166e190745b8d74e468f
zübeyir <zeynelzatari@gmail.com>
2022-07-23 22:38:49 +0200

    day_5 Deserializaiton Test 2  is done

--- src/test/java/com/cybertek/day5/JSONtoJAVATest.java
+++ src/test/java/com/cybertek/day5/JSONtoJAVATest.java
@@ -5,6 +5,7 @@ import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.Map;
 
 import static io.restassured.RestAssured.*;
@@ -33,5 +34,25 @@ public class JSONtoJAVATest extends SpartanTestBase {
 
     }
 
+    @DisplayName("GET all spartans to JAVA data structure")
+    @Test
+    public void getAllSpartanTest(){
+
+        Response response = get("/api/spartans").then().statusCode(200).extract().response();
+
+        //we need to convert json to java  which is deserialize
+
+        List<Map<String,Object>> jsonList = response.as(List.class); // burada tüm spartan 'i map in icine aldik amp icinde tekra map
+
+
+        //--jsonList.get(1).get("name")--> burada daha önce yaptigimiz items[1].names gibi birsey--> ama artik java da map a cevirdik
+        //onun icin ilk get array ikinci hangi element ise onu yaziyoruz
+        System.out.println(jsonList.get(1).get("name"));
+
+        Map<String,Object>spartan3 = jsonList.get(2);// burada sadec 3. siradki spartan 'i istedik ve onun icin index 2 yazdik get e
+        System.out.println("spartan3 = " + spartan3);
+
+
+    }
 
 }
